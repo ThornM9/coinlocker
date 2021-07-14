9772e57a16524f0dd73a4a9b983272794947db28
thornm9 <tm.jalal@gmail.com>
2021-07-14 14:34:56 +1000

    replace chainlink contract import with interface

--- contracts/EthLocker.sol
+++ contracts/EthLocker.sol
@@ -1,7 +1,6 @@
 pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 interface PriceFeedInterface {
     function latestRoundData() external view returns (uint80 roundId,int256 answer,uint256 startedAt,uint256 updatedAt,uint80 answeredInRound);
@@ -23,28 +22,22 @@ contract EthLocker is Ownable {
   mapping(uint => Deposit) private idToDeposit;
   PriceFeedInterface private priceFeed;
 
-  constructor() public {
+  constructor() {
     priceFeed = PriceFeedInterface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e); // ETH/USD price feed on rinkeby
   }
 
   function setPriceFeedAddress(address _priceFeedAddress) external onlyOwner {
-    priceFeed = AggregatorV3Interface(_priceFeedAddress);
+    priceFeed = PriceFeedInterface(_priceFeedAddress);
   }
 
   function getPrice() public view returns (uint) {
-    (
-        uint80 roundID, 
-        int price,
-        uint startedAt,
-        uint timeStamp,
-        uint80 answeredInRound
-    ) = priceFeed.latestRoundData();
+    (,int price,,,) = priceFeed.latestRoundData();
     return uint(price);
   }
 
   function createDeposit(uint _lockDays, uint _lockUntilPrice) external payable {
     require(msg.value > 0, "There was no ETH sent with this transaction to lock");
-    Deposit deposit = Deposit({
+    Deposit memory deposit = Deposit({
       depositor: msg.sender,
       id: nextId,
       lockedAt: block.timestamp,
@@ -68,20 +61,20 @@ contract EthLocker is Ownable {
 
 
   function canRedeemDeposit(uint id) public view returns (bool) {
-    Deposit deposit = idToDeposit[id];
+    Deposit memory deposit = idToDeposit[id];
     if (deposit.lockedAt + deposit.lockDays <= block.timestamp) { return false; }
     if (getPrice() <= deposit.lockUntilPrice) { return false; }
 
     return true;
   }
 
-  function _redeem(Deposit deposit) internal {
+  function _redeem(Deposit storage deposit) internal {
     deposit.redeemed = true; // prevent re-entrancy attack by redeeming before sending
     payable(deposit.depositor).transfer(deposit.amount);
   }
 
   function redeemDeposit(uint id) external {
-    Deposit deposit = idToDeposit[id];
+    Deposit storage deposit = idToDeposit[id];
     require(canRedeemDeposit(id), "Deposit cannot be redeemed yet");
     require(!deposit.redeemed, "Deposit has already been redeemed");
 
@@ -90,7 +83,7 @@ contract EthLocker is Ownable {
 
   function forceRedeem(uint id) external onlyOwner {
     // if a deposit gets stuck for some reason, the owner can redeem the deposit to the depositors address
-    Deposit deposit = idToDeposit[id];
+    Deposit storage deposit = idToDeposit[id];
     _redeem(deposit);
   }
 }
